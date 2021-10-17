d733aebe5a6c90b0d6435f3bbaca21731cd883a9
CaroBuenrostro19 <70624310+CaroBuenrostro19@users.noreply.github.com>
2021-10-16 22:01:10 -0500

    Actualizacion

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    //Variables
+    public static int countCoins=0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Coin.countCoins++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDestroy()
+    {
+        Coin.countCoins--;
+        if(Coin.countCoins<=0)
+        {
+            GameObject timer = GameObject.Find("GameTimer");
+            Destroy(timer);
+            Debug.Log("El juego ha terminado. ¡Has ganado!");
+        }
+    }
+
+    void OnTriggerEnter (Collider collider)
+    {
+        Debug.Log("Se ha recolectado la moneda #" + Coin.countCoins);
+        if (collider.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
